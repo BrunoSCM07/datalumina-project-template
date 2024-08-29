c69f3e88f6424a50145ee1900960d4abe1ea3efe
BrunoSCM07 <brunorafael@starchasermedia.com>
2024-08-29 08:09:31 -0600

    Second commit

--- data/data_prep.py
+++ data/data_prep.py
@@ -3,3 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
+
+df = pd.DataFrame({"C": [3, 4, 1], "D": [3, 4, 1]})
+
+df = pd.DataFrame({"E": [3, 4, 1], "F": [3, 4, 1]})
